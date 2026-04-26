2762592cffa3c59e4dafde1623a5393e92596f75
mariuste <mariustetard@googlemail.com>
2026-04-26 14:46:40 +0200

    tested, was sucessful, ready to tag

--- firmware/projects/RFIDBOX_PoC/include/settings.h
+++ firmware/projects/RFIDBOX_PoC/include/settings.h
@@ -7,8 +7,8 @@
  * cncmill-wood, cncmill-metal, 3dprinter-xl, panel-saw, wood-planer, 
  * wood-bandsaw, miter-saw, wood-routertable
  */
-#define MACHINE_NAME "3dprinter"   // Typ aus der Liste oben wählen
-#define MACHINE_ID   "Drucker3"     // Eindeutige Kennung dieser Hardware-Box
+//#define MACHINE_NAME "3dprinter"   // Typ aus der Liste oben wählen
+//#define MACHINE_ID   "Drucker3"     // Eindeutige Kennung dieser Hardware-Box
 #define RFIDCARD_AUTH_CONST true    // true = Card-Auth aktiv
 #define CONTINUOUS_SERVER_CHECK true // true = alle 2s Server-Auth-Check im RUNNING (nur mit RFIDCARD_AUTH_CONST)
 
