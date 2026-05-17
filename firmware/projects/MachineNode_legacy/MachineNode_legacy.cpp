e6cb6fd1bcd9cbdc3049344be6cef3f37d04420f
mariuste <mariustetard@googlemail.com>
2026-05-17 17:12:56 +0200

    compiled, läuft

--- firmware/projects/MachineNode_legacy/MachineNode_legacy.cpp
+++ firmware/projects/MachineNode_legacy/MachineNode_legacy.cpp
@@ -1,40 +1,34 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/pio.h"
 
-#include "blink.pio.h"
+#if defined(CYW43_WL_GPIO_LED_PIN)
+#include "pico/cyw43_arch.h"
+#endif
 
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-    blink_program_init(pio, sm, offset, pin);
-    pio_sm_set_enabled(pio, sm, true);
-
-    printf("Blinking pin %d at %d Hz\n", pin, freq);
-
-    // PIO counter program takes 3 more cycles in total than we pass as
-    // input (wait for n + 1; mov; jmp)
-    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
-}
-
-
-
-int main()
-{
-    stdio_init_all();
-
-    // PIO Blinking example
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &blink_program);
-    printf("Loaded program at %d\n", offset);
-    
-    #ifdef PICO_DEFAULT_LED_PIN
-    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
-    #else
-    blink_pin_forever(pio, 0, offset, 6, 3);
-    #endif
-    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
+int main() {
+#if defined(CYW43_WL_GPIO_LED_PIN)
+    if (cyw43_arch_init()) {
+        return 1;
+    }
 
     while (true) {
-        printf("Hello, world!\n");
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(500);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(500);
+    }
+#elif defined(PICO_DEFAULT_LED_PIN)
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+    while (true) {
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        sleep_ms(500);
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        sleep_ms(500);
+    }
+#else
+    while (true) {
         sleep_ms(1000);
     }
+#endif
 }
